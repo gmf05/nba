d15ee3c4216ecb82d9f1d2a767c81750e36043c8
Grant <gfiddyment@gmail.com>
2014-07-14 12:09:06 -0400

    Stable, polished version

--- nbastats.py
+++ nbastats.py
@@ -1,57 +1,47 @@
-import urllib2
+import sys
+import csv
 import json
-# import elementtree.ElementTree as ET
+import urllib2
 import scipy.io as spio
-import numpy as np
+import numpy 
 
-API_KEY = "YOUR_API_KEY"
-sqdl_call = "date,team,o:team,points,margin,margin after the first, margin at the half, margin after the third,biggest lead,field goals attempted, field goals made, three pointers attempted, three pointers made, free throws attempted, free throws made,points in the paint,assists,steals,blocks,offensive rebounds,defensive rebounds,turnovers"
-features = sqdl_call.split(",")
-# sqdl_html=sqdl_call + "@season=YYYY and game number=NNNN"
-sqdl_html=sqdl_call + "@season=YYYY"
-replace_table = {",":"%2C", ":":"%3A", " ":"+", "=":"%3D", "@":"%40"}
-for n in replace_table:
-  sqdl_html = sqdl_html.replace(n,replace_table[n])
-urlroot = "http://api.sportsdatabase.com/nba/query.json?sdql=" + sqdl_html + "&output=json&api_key=" + API_KEY
+sqdl_features = "date,team,o:team,points,margin,margin after the first,margin at the half,margin after the third,biggest lead,field goals attempted,field goals made,three pointers attempted,three pointers made,free throws attempted,free throws made,points in the paint,assists,steals,blocks,offensive rebounds,defensive rebounds,turnovers"
+features = sqdl_features.split(",")
+urlroot = "http://api.sportsdatabase.com/nba/query.json?sdql=SQDL&output=json&api_key=YOUR_API_KEY"
+
+def sqdlHTML(sqdl):
+  replace_table = {",":"%2C", ":":"%3A", " ":"+", "=":"%3D", "@":"%40"}
+  html = sqdl
+  for n in replace_table:
+    html = html.replace(n,replace_table[n])
+  return html
 
 def getSeason(yyyy):
-  games = []
-  url = urlroot.replace("YYYY", str(yyyy))
-  F = urllib2.urlopen(url)
-  J = F.read()    
-  F.close()
+  sqdl_html = sqdlHTML(sqdl_features + "@season=" + str(yyyy))
+  url = urlroot.replace("SQDL",sqdl_html)
+  FR = urllib2.urlopen(url)
+  J = FR.read()    
+  FR.close()
   J = J.replace("json_callback(","") # removing junk text
   J = J.replace("});","}") # removing junk text
   J = J.replace("\'", "\"") # switching to double quotes throughout
   J0 = json.loads(J)
-  features = J0['headers']
   data = J0['groups'][0]['columns']
-  Nsamples = len(data[0])
+  data =  numpy.asarray(data).T.tolist()
+  return data
 
-#     N = len(data[0])
-#     # loop over each game, add data to list
-#         game_tree = ET.XML(game_str)
-#       
-#      games.append({
-#       'league': league,
-#       'start': start,
-#        'home': home,
-#        'away': away,
-#        'home-score': home_tree.get('score'),
-#        'away-score': visiting_tree.get('score'),
-#        'status': gamestate_tree.get('status'),
-#        'clock': gamestate_tree.get('display_status1'),
-#        'clock-section': gamestate_tree.get('display_status2')
-#      })
-      # for loop:
-#       games.append({
-#         'Win': wins, 
-#         'Points': pts
-#         })
-  return data,features
+def csvSeason(yyyy):
+  games = getSeason(yyyy)
+  with open('NBASeason' + str(yyyy) + '.csv','w') as csvfile:
+    csvWriter = csv.writer(csvfile)
+    csvWriter.writerow(features)
+    for g in games:
+      csvWriter.writerow(g)
+    csvfile.close()
 
 if __name__ == "__main__":
-  season = 2013
-  (G,F) = getSeason(season)
+  yyyy = sys.argv[1]
+  (G,F) = getSeason(yyyy)
   D = {"games":G, "features":F}
   spio.matlab.savemat('NBASeason' + str(season), D)
+#   csvSeason(yyyy)
