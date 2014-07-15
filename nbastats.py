ac629a5bc38a154e1017016c451eb627ee4b2170
Grant <gfiddyment@gmail.com>
2014-07-14 22:15:46 -0400

    Touched up code, adding player stats

--- nbastats.py
+++ nbastats.py
@@ -5,30 +5,55 @@ import urllib2
 import scipy.io as spio
 import numpy 
 
-sqdl_features = "date,team,o:team,points,margin,margin after the first,margin at the half,margin after the third,biggest lead,field goals attempted,field goals made,three pointers attempted,three pointers made,free throws attempted,free throws made,points in the paint,assists,steals,blocks,offensive rebounds,defensive rebounds,turnovers"
-features = sqdl_features.split(",")
+team_features = "date,team,o:team,points,margin,margin after the first,margin at the half,margin after the third,biggest lead,field goals attempted,field goals made,three pointers attempted,three pointers made,free throws attempted,free throws made,points in the paint,assists,steals,blocks,offensive rebounds,defensive rebounds,turnovers"
+player_features="o:team,assists,blocks,defensive rebounds,field goals attempted,field goals made,fouls,free throws attempted,free throws made,minutes,offensive rebounds,points,rebounds,steals,three pointers attempted,three pointers made,turnovers"
 urlroot = "http://api.sportsdatabase.com/nba/query.json?sdql=SQDL&output=json&api_key=YOUR_API_KEY"
 
-def sqdlHTML(sqdl):
+def formatSQDL(sqdl):
   replace_table = {",":"%2C", ":":"%3A", " ":"+", "=":"%3D", "@":"%40"}
   html = sqdl
   for n in replace_table:
     html = html.replace(n,replace_table[n])
   return html
 
-def getSeason(yyyy):
-  sqdl_html = sqdlHTML(sqdl_features + "@season=" + str(yyyy))
-  url = urlroot.replace("SQDL",sqdl_html)
-  FR = urllib2.urlopen(url)
-  J = FR.read()    
-  FR.close()
-  J = J.replace("json_callback(","") # removing junk text
-  J = J.replace("});","}") # removing junk text
+def formatJSONP(jsonp):
+  J = jsonp.replace("json_callback(","").replace("});","}") # remove padding
   J = J.replace("\'", "\"") # switching to double quotes throughout
-  J0 = json.loads(J)
-  data = J0['groups'][0]['columns']
-  data =  numpy.asarray(data).T.tolist()
-  return data
+  J = json.loads(J)
+  J = J['groups'][0]['columns']
+  J =  numpy.asarray(J).T.tolist()
+  # remove unplayed games (needs to be itereated for some reason...)
+  for n in range(2):
+    for j in J:
+      if not any(j[3::]):
+        print j
+        J.remove(j)
+  return J
+
+def getSeason(yyyy):
+  features = team_features.split(",")
+  sqdl_html = formatSQDL(team_features + "@season=" + str(yyyy))
+  url = urlroot.replace("SQDL",sqdl_html)
+  F = urllib2.urlopen(url)
+  data = formatJSONP(F.read())
+  F.close()
+  return data,features
+
+def getPlayerSeason(player, team, yyyy):
+  pf = "date," + player_features.replace(",", "," + player + ":")  
+  sqdl_html = formatSQDL(pf + "@" + team + ":season=" + str(yyyy))
+  sqdl_call = pf + "@" + team + ":season=" + str(yyyy)
+  url = urlroot.replace("SQDL",sqdl_html)
+  F = urllib2.urlopen(url)
+  data = formatJSONP(F.read())
+  F.close()
+  # save to .mat file
+  features = pf.replace(player + ":","").split(",") 
+  stats =  numpy.asarray(data,dtype=numpy.object)
+  D = {"stats":stats, "features":features}
+  matfile = player.replace(" ","-") + "-" + str(yyyy) + ".mat"
+  spio.matlab.savemat(matfile, D)
+#   return data,features
 
 def csvSeason(yyyy):
   games = getSeason(yyyy)
@@ -55,6 +80,7 @@ def xlsSeason(yyyy):
 
 if __name__ == "__main__":
   yyyy = sys.argv[1]
-#   csvSeason(yyyy) # works
-#   xlsSeason(yyyy) # doesn't work
-  matSeason(yyyy) # works
+#   csvSeason(sys.argv[1]) # works
+#   xlsSeason(sys.argv[1]) # doesn't work
+#   matSeason(sys.argv[1]) # works
+  getPlayerSeason(sys.argv)
