5331ae4c6e0a6f6934b472e7e6304d2753c8e87d
Grant Fiddyment <gfiddy@cns-ws27.bu.edu>
2015-03-03 11:39:54 -0500

    Adding bash script for running loop over all teams

---
+++ run_team_batch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+declare -a teams=("ATL" "BOS" "BKN" "CHI" "CHA" "CLE" "DAL" "DEN" "DET" "GSW" "HOU" "IND" "LAC" "LAL" "MEM" "MIA" "MIL" "MIN" "NOP" "NYK" "OKC" "ORL" "PHI" "PHX" "POR" "SAC" "SAS" "TOR" "UTA" "WAS")
+# echo ${#teams[@]} # check number of teams
+for i in "${teams[@]}"
+do
+	echo $i
+  ./nbastats.py $i
+done
+
+# keep this line to ensure newline
+
