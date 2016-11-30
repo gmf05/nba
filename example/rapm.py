813842c25e5dfdedc2fb857f098754eac7ce051c
Grant F <gfiddyment@gmail.com>
2016-11-30 16:15:43 -0500

    Tidied up example

--- example/rapm.py
+++ example/rapm.py
@@ -22,8 +22,9 @@ events_db = db['possessions']
 
 # only need once:
 #import pymongo
-#events_db.create_index([('SEASONID', pymongo.ASCENDING), ('GAMEID', pymongo.ASCENDING)])
+#events_db.create_index([('SEASON_ID', pymongo.ASCENDING), ('GAME_ID', pymongo.ASCENDING)])
 
+#season_year = '1996-97'
 #season_year = '2014-15'
 season_year = '2015-16'
 
@@ -33,83 +34,101 @@ teams = bb.get_teams_current()
 Nplayers = len(all_players)
 Nteams = len(teams)
 
-#%% Fit league RPM model
+#%% Fit team RPM models
 
-#def fit_league_rpm():
-Nplayers = len(all_players)
-player_dict = bb.zip2(all_players['PERSON_ID'].values, range(Nplayers)) 
-
-# Offense
-possessions = events_db.find({'$and': [{'SEASONID': seasonid}, {'EVENTTYPE':{'$in':[1,2,3,5]}}, {'SCORECHANGE': {'$gte': 0}}, {'SCORECHANGE': {'$lte': 3}}]},
-                    {'OFFPLAYERS':1, 'DEFPLAYERS':1, 'SCORECHANGE':1, 'POSSTYPE':1}
-                    )
-N_poss = possessions.count()
-X_off = np.zeros([N_poss, Nplayers])
-X_def = np.zeros([N_poss, Nplayers])
-y = np.zeros([N_poss])
-z = np.zeros([N_poss])
+def fit_team_rpm(team_id):
+  team_players = bb.get_team_roster(team_id, season=season_year)
+  Nteam_players = len(team_players)
+  team_dict = bb.zip2(team_players['PLAYER_ID'].values, range(Nteam_players))
   
+  # Offense
+  off_possessions = events_db.find({'$and': [{'SEASON_ID': seasonid}, {'OFF_TEAM':team_id}]},
+                      {'OFF_PLAYERS':1, 'DEF_PLAYERS':1, 'SCORE_CHANGE':1, 'DURATION':1}
+                      )
+  N_off = off_possessions.count()
+  X_off = np.zeros([N_off, Nteam_players])
+  y_off = np.zeros([N_off])
+  
+  i=0
+  for s in off_possessions:
+    
+    try:
+      o_idx = [team_dict[p] for p in s['OFF_PLAYERS']]
+    except:
+      S = np.copy(s['OFF_PLAYERS'])
+      S = S[S>0]
+      S = np.setdiff1d(S, S[np.where([p not in team_dict.keys() for p in S])[0]])
+      o_idx = [team_dict[p] for p in S]
+    
+    # If poss_type == 2: change i
+    # 
+    X_off[i, o_idx] = 1
+    y_off[i] += s['SCORE_CHANGE']
+    i+=1
+    
+  m_off = slm.Ridge(alpha=3000)
+  m_off.fit_intercept = False
+  m_off = m_off.fit(X_off, y_off)
+  
+  # Defense
+  def_possessions = events_db.find({'$and': [{'SEASON_ID': seasonid}, {'DEF_TEAM':team_id}]},
+                      {'OFF_PLAYERS':1, 'DEF_PLAYERS':1, 'SCORE_CHANGE':1, 'DURATION':1}
+                      )
+  N_def = def_possessions.count()
+  X_def = np.zeros([N_def, Nteam_players])
+  y_def = np.zeros([N_def])
+  
+  i=0
+  for s in def_possessions:
+    
+    try:
+      d_idx = [team_dict[p] for p in s['DEF_PLAYERS']]
+    except:
+      S = np.copy(s['DEF_PLAYERS'])
+      S = S[S>0]
+      S = np.setdiff1d(S, S[np.where([p not in team_dict.keys() for p in S])[0]])
+      d_idx = [team_dict[p] for p in S]
+      
+    X_def[i, d_idx] = -1
+    y_def[i] = s['SCORE_CHANGE']
+    i+=1
+  
+  m_def = slm.Ridge(alpha=3000)
+  m_def.fit_intercept = False
+  m_def = m_def.fit(X_def, y_def)
+
+  team_players['ORtg'] = np.round( m_off.coef_ * 100, 2)
+  team_players['DRtg'] = np.round( m_def.coef_ * 100, 2)
+  team_players['NetRtg'] = team_players['ORtg'] + team_players['DRtg']
+  team_players['O_Poss'] = np.sum(X_off,0)
+  team_players['D_Poss'] = np.sum(np.abs(X_def),0)
+  team_players['Points_Est'] = np.round(team_players['ORtg']/100.0 * team_players['O_Poss'])
+  team_players['Points_Opp'] = np.round(team_players['DRtg']/100.0 * team_players['D_Poss'])
+  team_players['Net_Points'] = team_players['Points_Est'] + team_players['Points_Opp']
+  
+  #return team_players.sort(columns='Net_Points',ascending=False)[['PLAYER', 'ORtg', 'DRtg', 'NetRtg', 'O_Poss', 'D_Poss', 'Net_Points']]
+  #return team_players.sort(columns='Net_Points',ascending=False)[['PLAYER', 'ORtg', 'DRtg', 'NetRtg', 'O_Poss', 'Points_Est', 'Net_Points']]
+  return team_players.sort(columns='Net_Points',ascending=False)[['PLAYER', 'ORtg', 'DRtg', 'NetRtg', 'O_Poss', 'Points_Est','Net_Points']]
+
+#%% Print models
+
+m = pd.DataFrame()
 i=0
-for s in possessions:
-  
-  #if np.mod(i,1e3)==1: print i
-    #t1 = time.time()
-    #print i,t1-t0
-    #t0 = t1
-  
-  try:
-    o_idx = [player_dict[p] for p in s['OFFPLAYERS']]
-  except:
-    S = np.copy(s['OFFPLAYERS'])
-    S = S[S>0]
-    S = np.setdiff1d(S, S[np.where([p not in player_dict.keys() for p in S])[0]])
-    o_idx = [player_dict[p] for p in S]
-
-  try:
-    d_idx = [player_dict[p] for p in s['DEFPLAYERS']]
-  except:
-    S = np.copy(s['DEFPLAYERS'])
-    S = S[S>0]
-    S = np.setdiff1d(S, S[np.where([p not in player_dict.keys() for p in S])[0]])
-    d_idx = [player_dict[p] for p in S]
-
-  X_off[i, o_idx] = 1      
-  X_def[i, d_idx] = -1      
-  y[i] = s['SCORECHANGE']
-  z[i] = s['POSSTYPE']
+for team_id in teams['TEAM_ID'].values:
+  print '\n'
+  m1 = fit_team_rpm(team_id)
+  print m1
+  m = pd.concat((m,m1))
   i+=1
+m = m.reset_index()
 
-#m = slm.LassoLarsCV() # L1 reg
-m = slm.ElasticNet(l1_ratio = 0.8, alpha=4.5e-5) # L1-L2 combo reg
-#m.fit_intercept = True
-m.fit_intercept = False
-#m = m.fit(X_off, y)
-m = m.fit(np.hstack((X_off, X_def)), y)
+# Top 60 players in each category
+#print m[m['O_Poss']>=2000].sort(columns='ORtg',ascending=False)[0:60] 
+#print m[m['O_Poss']>=2000].sort(columns='DRtg',ascending=False)[0:60]
+#print m[m['O_Poss']>=2000].sort(columns='Net_Points',ascending=False)[0:60]
 
-rpm_o = m.coef_[0:Nplayers]
-rpm_d = m.coef_[Nplayers:]
-all_players['ORPM'] = np.round( rpm_o * 100, 1)
-all_players['DRPM'] = np.round( rpm_d * 100, 1)
-all_players['RPM'] = all_players['ORPM'] + all_players['DRPM']
-#all_players['O_Poss'] = np.sum(X_off,0)
-#all_players['D_Poss'] = np.sum(np.abs(X_def),0)
-#all_players['Poss'] = 0.5*(all_players['O_Poss'] + all_players['D_Poss'])
-#all_players['Points'] = all_players['ORPM']/100.0 * all_players['O_Poss']
-# USE Z TO ESTIMATE  O/D Possession totals
-#all_players['MinEst'] = np.round(all_players['Poss']/110.0 * 48.0)
-#all_players['Net_Points'] = all_players['RPM']/100.0 * (all_players['O_Poss'] + all_players['D_Poss'])
-#all_players['Net_Points'] = np.round(all_players['ORPM']/100.0 * all_players['O_Poss'] + all_players['DRPM']/100.0 * all_players['D_Poss'], 1)
-#all_players['PPG'] = all_players['Poss'] / 82.0
-#all_players['O_PPG'] = all_players['ORPM'] /100.0 * all_players['Poss']
-#all_players['D_PPG'] = all_players['DRPM'] /100.0 * all_players['Poss']
-#all_players['Net_PPG'] = all_players['O_PPG'] + all_players['D_PPG'] 
-#all_players['Value'] = all_players['Net_Points'] / all_players['Salary']
+# Bottom 60 players in each category
+#print m[m['O_Poss']>=2000].sort(columns='ORtg',ascending=True)[0:60]
+#print m[m['O_Poss']>=2000].sort(columns='DRtg',ascending=True)[0:60]
+#print m[m['O_Poss']>=2000].sort(columns='Net_Points',ascending=True)[0:60]
 
-#print all_players.sort(columns='Net_Points',ascending=False)[['DISPLAY_FIRST_LAST', 'ORPM', 'DRPM', 'RPM', 'MinEst', 'Net_Points']][0:60]
-#print all_players.sort(columns='Value',ascending=False)[['DISPLAY_FIRST_LAST', 'RPM','Net_Points', 'Salary', 'Value']][0:60]
-#print all_players.sort(columns='Value',ascending=False)[['DISPLAY_FIRST_LAST', 'Poss', 'Net_PPG', 'Salary']][0:60]
-#print all_players.sort(columns='RPM',ascending=False)[['DISPLAY_FIRST_LAST', 'ORPM', 'DRPM', 'RPM', 'Poss', 'Points']][0:60]
-#print all_players.sort(columns='Points',ascending=False)[['DISPLAY_FIRST_LAST', 'ORPM', 'DRPM', 'RPM', 'MinEst', 'Points']][0:60]
-print all_players[all_players['Poss']>=1000].sort(columns='RPM',ascending=False)[['DISPLAY_FIRST_LAST', 'ORPM', 'DRPM', 'RPM', 'MinEst', 'Points']][0:60]
-
-#%%
